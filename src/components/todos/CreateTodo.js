11a31337030bd5b714e97f37588bc30b423f7766
ghemsley <ghemsley@protonmail.ch>
2021-07-27 15:54:23 -0500

    commit tree 0313849402c1d5ba76533eaab9fa8a1aa570200d

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -2,35 +2,41 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class CreateTodo extends Component {
-
   constructor() {
-    super();
+    super()
     this.state = {
-      text: '',
-    };
+      text: ''
+    }
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault()
     this.props.addTodo(this.state)
+    this.setState({
+      text: ''
+    })
   }
 
   handleChange(event) {
     this.setState({
       text: event.target.value
-    });
+    })
   }
 
   render() {
-    return(
+    return (
       <div>
         <form onSubmit={this.handleSubmit}>
-    	    <label>add todo</label>
-          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
-          <input type="submit" />
-       </form>
-     </div>
-   );
+          <label>add todo</label>
+          <input
+            type='text'
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.text}
+          />
+          <input type='submit' />
+        </form>
+      </div>
+    )
   }
 };
 
